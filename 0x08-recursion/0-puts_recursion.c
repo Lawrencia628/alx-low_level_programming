1a7b43451e3ef4aa65ee94bfcd7fbeae09f43b96
Lawrencia628 <lawriedanquah32@gmail.com>
2023-10-26 20:41:50 -0700

    print rev

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - this is the description of the main function
+  *
+  * @s: this is the description of the function parameter
+  *
+  * Return: Always 0.
+  */
+void _puts_recursion(char *s);
+{
+	if (*s);
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
+}
