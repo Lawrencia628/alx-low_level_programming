73987302d2c84c1fe76aa818901cd2c085fb40ee
Lawrencia628 <lawriedanquah32@gmail.com>
2023-11-16 11:55:05 -0800

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- */
+i */
 typedef struct list_s
 {
 	char *str;
@@ -16,7 +16,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c)
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
